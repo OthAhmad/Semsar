c872849ac0aea22d22281436f8d00488db42da7e
Ahmad <yoi@example.com>
2018-09-22 17:51:31 +0200

    server

--- server.py
+++ server.py
@@ -3,10 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/.well-known/acme-challenge/Lfmxqpq_lxKo2s8jJK_ujsiKtO8H7dhDjjOiKI8OeAs")
+@app.route("/.well-known/acme-challenge/EGMBUzX4gwX09U3JBHNvdMc8ymU3JNXH0HwC9LEB5qY")
 # e.g. /.well-known/acme-challenge/lzrajCaq8vbw5Qz2o_XXXXXXXXXXXXXXXXX
 def hello():
-    return"Lfmxqpq_lxKo2s8jJK_ujsiKtO8H7dhDjjOiKI8OeAs.0PJTmSXcBn1hGck1NEMfzicOGKIdXuyTRMIJRzMzZIk"
+    return"EGMBUzX4gwX09U3JBHNvdMc8ymU3JNXH0HwC9LEB5qY.0PJTmSXcBn1hGck1NEMfzicOGKIdXuyTRMIJRzMzZIk"
     # e.g. return "lzrajCaq8vbw5Qz2o_XXXXXXXXXXXXXXXXXXz4RnfWtLKaIc6EdhsOsr4fb6RFZuUoabZW5dPW36cmc"
 
 if __name__ == "__main__":
